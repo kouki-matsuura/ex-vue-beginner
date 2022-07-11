8e70ce1085d3d51f449f9e593ef63bbf8152045a
koki.matsura <koki.matsura@B0365-MACB.local>
2022-07-11 10:31:04 +0900

    Ex04の実装

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/ex01">Ex01</router-link> |
       <router-link to="/ex02">Ex02</router-link> |
-      <router-link to="/ex03">Ex03</router-link>
+      <router-link to="/ex03">Ex03</router-link> |
+      <router-link to="/ex04">Ex04</router-link>
     </nav>
     <router-view />
   </div>
---
+++ src/views/Ex04.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" v-model.number="num1" /><br />
+    <input type="text" v-model.number="num2" /><br />
+    <input type="text" v-model.number="num3" /><br />
+    税抜き価格:{{ price.toLocaleString() }}円
+    <br />
+    税込価格：{{ priceWithTax.toLocaleString() }}円
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Ex04Component extends Vue {
+  private num1 = 0;
+  private num2 = 0;
+  private num3 = 0;
+
+  get price() {
+    return this.num1 + this.num2 + this.num3;
+  }
+
+  get priceWithTax() {
+    return Math.floor(this.price * 1.1);
+  }
+}
+</script>
+
+<style scoped></style>
